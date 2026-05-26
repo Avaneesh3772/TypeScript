af58b597a1ec2f27d810dfaaab3482924f8841c6
Avaneesh Mishra <avaneesh.3772@gmail.com>
2026-05-26 19:04:38 +0530

    Initial TypeScript Commit

---
+++ Backup/3rd Jan/public/classes/app.js
@@ -0,0 +1,7 @@
+"use strict";
+var anchor = document.querySelector('a');
+if (anchor) {
+    console.log(anchor.href);
+}
+var form = document.querySelector('.new-item-form');
+console.log(form.children);
