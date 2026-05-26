0178268ab8e24d3a40fef4ce283ffae98861d9d1
Avaneesh Mishra <avaneesh.3772@gmail.com>
2026-05-26 19:06:29 +0530

    Typescript code chnages

--- Backup/3rd Jan/public/classes/app.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-var anchor = document.querySelector('a');
-if (anchor) {
-    console.log(anchor.href);
-}
-var form = document.querySelector('.new-item-form');
-console.log(form.children);
